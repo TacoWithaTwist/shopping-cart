d27a0dfa161dda2f32c528583a26a7a047da7ff7
Houssein Windows <houssein.msallem@gmail.com>
2024-02-11 11:32:55 +0100

    yes?

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,21 +1,8 @@
 import NavBar from "./NavBar.jsx";
 import useFetchShop from "./useFetchShop.jsx";
 import "../cssModules/Shop.css";
-import ErrorPage from "./ErrorPage.jsx";
-import ItemColumn from "./ItemColumn.jsx";
-export default function Shop({ Items }) {
-  const { ShopArr, error } = useFetchShop(Items);
-
-  if (error) {
-    return <ErrorPage />;
-  }
-
-  // Split ShopArr into columns
-  const columnCount = 4; // Change this value as needed
-  const columns = Array.from({ length: columnCount }, (_, i) =>
-    ShopArr.filter((_, idx) => idx % columnCount === i)
-  );
-
+const ShopArr = await FakeStoreAPI();
+export default function Shop() {
   return (
     <>
       <NavBar />
--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
 import "../cssModules/Item.css";
 import AddToCartButton from "./AddToCartButton";
+import { useContext } from "react";
 function ShopItem(props) {
+  const { addToCart } = useContext(ShopContext);
+  const handleAddToCart = () => {
+    addToCart({
+      ItemName: props.ItemName,
+      ItemPrice: props.ItemPrice,
+      ItemImage: props.ItemImage,
+    });
+  };
   return (
     <div className={props.className}>
       <img src={props.ItemImage} alt="" />
       <h3>{props.ItemName}</h3>
       <span>{props.ItemPrice}</span>
       <br />
-      <AddToCartButton />
+      <AddToCartButton onClick={handleAddToCart()} />
     </div>
   );
 }
